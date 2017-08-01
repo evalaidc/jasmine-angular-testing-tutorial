3e2e878ff21a6350afbbd126f4e7bdd01efb302d
Eva Lai <evalai.dc@gmail.com>
2017-08-01 08:37:13 -0400

    completed express/server file

--- server.js
+++ server.js
@@ -14,3 +14,15 @@ app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
   next();
 });
+
+app.use(morgan(dev));
+app.use(express.static(__dirname + '/app'));
+
+// sends our app to index.html in our director.
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// sets where the local host location or where our appliaction will be
+app.listen(8080);
+console.log('meet-irl is running on 8080');
