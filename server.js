2ea472fe5a8be6113093976a4273f01dd9567740
Eva Lai <evalai.dc@gmail.com>
2017-08-01 01:02:39 -0400

    added body parser and convert to json

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    path = require('path');
+
+app.use(bodyParser.urlencoded({extended: true}));
