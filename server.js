f2548129842ca3cc7beb12c16a4e0cefa5465480
Eva Lai <evalai.dc@gmail.com>
2017-08-01 08:33:14 -0400

    added access controll

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ var express = require('express'),
     path = require('path');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// allows access to servers
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
